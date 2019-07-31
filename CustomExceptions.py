6265d981500d575efb0cfed3d8a79afe872c2b6c
sgibson91 <sgibson@turing.ac.uk>
2019-07-31 11:33:17 +0100

    Add Azure Error

--- CustomExceptions.py
+++ CustomExceptions.py
@@ -1,7 +1,11 @@
-class GitError(Exception):
+class AzureError(Exception):
     def __init__(self, *args, **kwargs):
         Exception.__init__(self, *args, **kwargs)
 
 class BashError(Exception):
+    def __init__(self, *args, **kwargs):
+        Exception.__init__(self, *args, **kwargs)
+
+class GitError(Exception):
     def __init__(self, *args, **kwargs):
         Exception.__init__(self, *args, **kwargs)
