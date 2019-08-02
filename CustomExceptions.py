2b28719cceaca9e01d06fcb3d9101bf31481f200
sgibson91 <sgibson@turing.ac.uk>
2019-08-02 13:19:19 +0100

    Remove bash exception

--- CustomExceptions.py
+++ CustomExceptions.py
@@ -2,10 +2,6 @@ class AzureError(Exception):
     def __init__(self, *args, **kwargs):
         Exception.__init__(self, *args, **kwargs)
 
-class BashError(Exception):
-    def __init__(self, *args, **kwargs):
-        Exception.__init__(self, *args, **kwargs)
-
 class GitError(Exception):
     def __init__(self, *args, **kwargs):
         Exception.__init__(self, *args, **kwargs)
