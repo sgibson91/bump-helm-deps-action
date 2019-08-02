515fad2f4d59670e8e851fb5c8888ef903681680
sgibson91 <sgibson@turing.ac.uk>
2019-08-02 13:19:40 +0100

    Abstract out Popen call

---
+++ run_command.py
@@ -0,0 +1,28 @@
+import subprocess
+
+def run_cmd(cmd):
+    """Use Popen to run a subprocess command.
+
+    Parameters
+    ----------
+    cmd: List of strings
+
+    Returns
+    -------
+    result: Dictionary
+    """
+    proc = subprocess.Popen(
+        cmd,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE
+    )
+
+    msgs = proc.communicate()
+
+    result = {
+        "returncode": proc.returncode,
+        "output": msgs[0].decode(encoding=("utf-8")),
+        "err_msg": msgs[1].decode(encoding=("utf-8")),
+    }
+
+    return result
