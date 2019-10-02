e9b1fbb24f272efd6d6fd36335e9fc193a98558c
Sarah Gibson <sgibson@turing.ac.uk>
2019-10-02 10:56:33 +0100

    Black linting

--- run_command.py
+++ run_command.py
@@ -1,5 +1,6 @@
 import subprocess
 
+
 def run_cmd(cmd):
     """Use Popen to run a subprocess command.
 
@@ -12,9 +13,7 @@ def run_cmd(cmd):
     result: Dictionary
     """
     proc = subprocess.Popen(
-        cmd,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE
+        cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE
     )
 
     msgs = proc.communicate()
