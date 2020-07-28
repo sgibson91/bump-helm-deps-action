97633c957a3ee6b3e0b276b539ac80ac09b2f4d3
Sarah Gibson <sgibson@turing.ac.uk>
2020-07-28 16:12:02 +0100

    Import all functions from github.py

--- HelmUpgradeBot/__init__.py
+++ HelmUpgradeBot/__init__.py
@@ -1,5 +1,18 @@
 __version__ = "0.0.1"
 
+from .github import (
+    add_commit_push,
+    add_labels,
+    check_fork_exists,
+    delete_old_branch,
+    checkout_branch,
+    clone_fork,
+    create_pr,
+    make_fork,
+    remove_fork,
+    set_github_config,
+)
+
 from .helper_functions import (
     delete_request,
     get_request,
