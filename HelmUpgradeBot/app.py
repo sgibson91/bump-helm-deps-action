fdb8188911fe0b1811b37a4f3e34a2547ba29d5c
Sarah Gibson <sgibson@turing.ac.uk>
2020-07-28 15:02:58 +0100

    Add get_chart_versions func with placeholders

--- HelmUpgradeBot/app.py
+++ HelmUpgradeBot/app.py
@@ -15,7 +15,7 @@ logger = logging.getLogger()
 
 
 def check_versions(
-    deployment: str, chart_info: dict, dry_run: bool = False
+    chart_name: str, chart_info: dict, dry_run: bool = False
 ) -> list:
     charts = list(chart_info.keys())
     charts.remove(deployment)
@@ -23,7 +23,7 @@ def check_versions(
     condition = [
         (
             chart_info[chart]["version"]
-            != chart_info[deployment][chart]["version"]
+            != chart_info[chart_name][chart]["version"]
         )
         for chart in charts
     ]
@@ -42,7 +42,7 @@ def check_versions(
     else:
         logger.info(
             "%s is up-to-date with all current chart dependency releases!"
-            % deployment
+            % chart_name
         )
 
     return charts_to_update
@@ -58,3 +58,29 @@ def clean_up(repo_name: str) -> None:
         logger.info("Deleting local repository: %s" % repo_name)
         shutil.rmtree(repo_name)
         logger.info("Deleted local repository: %s" % repo_name)
+
+
+def get_chart_versions(chart_name: str, repo_owner: str, repo_name: str):
+    chart_info = {}
+    chart_urls = {
+        chart_name: f"https://raw.githubusercontent.com/{repo_owner}/{repo_name}/main/{chart_name}/requirements.yaml",
+        "binderhub": "https://raw.githubusercontent.com/jupyterhub/helm-chart/gh-pages/index.yaml",
+        "nginx-ingress": "https://raw.githubusercontent.com/helm/charts/master/stable/nginx-ingress/Chart.yaml",
+    }
+
+    for (chart, chart_url) in chart_urls.items():
+        if "requirements.yaml" in chart_url:
+            # Get the versions from a requirements.yaml file
+            pass
+        elif "Chart.yaml" in chart_url:
+            # Get the versions from a Chart.yaml file
+            pass
+        elif "/gh-pages/" in chart_url:
+            # Get the versions from a gh-pages page
+            pass
+        else:
+            msg = "Scraping from the following URL type is currently not implemented\n\t%s" % chart_url
+            logger.error(NotImplementedError(msg))
+            raise NotImplementedError(msg)
+
+    return chart_info
