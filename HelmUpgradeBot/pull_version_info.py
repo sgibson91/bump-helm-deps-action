6b517dab0dee81a61ac8dd8de513d6b7fdacb819
Sarah Gibson <sgibson@turing.ac.uk>
2020-07-28 19:43:47 +0100

    Fix bugs in pull_version_info.py

--- HelmUpgradeBot/pull_version_info.py
+++ HelmUpgradeBot/pull_version_info.py
@@ -4,25 +4,27 @@ from .helper_functions import get_request
 
 
 def pull_version_from_requirements_file(
-    output_dict: dict, dependency: str, filename: str
+    output_dict: dict, chart_name: str, url: str, token: str
 ) -> dict:  # noqa: E501
     """Pull dependency versions requirements.yml file.
 
     Args:
         output_dict (dict): The dictionary to store versions in
-        dependency (str): The dependency to get a new version for
+        chart_name (str): The name of the helm chart
         url (str): The URL of the remotely hosted versions
+        token (str): A GitHub API token
     """
-    with open(filename, "r") as stream:
-        chart_reqs = yaml.safe_load(stream)
+    header = {"Authorization": f"token {token}"}
+    chart_reqs = yaml.safe_load(get_request(url, headers=header, text=True))
 
-    output_dict[dependency] = chart_reqs[dependency]["version"]
+    for chart in chart_reqs["dependencies"]:
+        output_dict[chart_name][chart["name"]] = chart["version"]
 
     return output_dict
 
 
 def pull_version_from_chart_file(
-    output_dict: dict, dependency: str, url: str
+    output_dict: dict, dependency: str, url: str, token: str
 ) -> dict:  # noqa: E501
     """Pull recent, up-to-date version from remote host stored in a Chart.yml
     file.
@@ -31,15 +33,17 @@ def pull_version_from_chart_file(
         output_dict (dict): The dictionary to store versions in
         dependency (str): The dependency to get a new version for
         url (str): The URL of the remotely hosted versions
+        token (str): A GitHub API token
     """
-    chart_reqs = yaml.safe_load(get_request(url, text=True))
+    header = {"Authorization": f"token {token}"}
+    chart_reqs = yaml.safe_load(get_request(url, headers=header, text=True))
     output_dict[dependency] = chart_reqs["version"]
 
     return output_dict
 
 
 def pull_version_from_github_pages(
-    output_dict: dict, dependency: str, url: str
+    output_dict: dict, dependency: str, url: str, token: str
 ) -> dict:
     """Pull recent, up-to-date version from remote host listed on a GitHub Pages
     site.
@@ -48,8 +52,10 @@ def pull_version_from_github_pages(
         output_dict (dict): The dictionary to store versions in
         dependency (str): The dependency to get a version for
         url (str): The URL of the remotely hosted versions
+        token (str): A GitHub API token
     """
-    chart_reqs = yaml.safe_load(get_request(url, text=True))
+    header = {"Authorization": f"token {token}"}
+    chart_reqs = yaml.safe_load(get_request(url, headers=header, text=True))
     updates_sorted = sorted(
         chart_reqs["entries"][dependency], key=lambda k: k["created"]
     )
