9a59c8d77b7380578f8be348dc951af2af6a495a
Sarah Gibson <sgibson@turing.ac.uk>
2020-07-29 07:47:02 +0100

    Test requests.get

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -2,7 +2,7 @@ import pytest
 import logging
 import responses
 from testfixtures import log_capture
-from helm_bot.helper_functions import delete_request
+from helm_bot.helper_functions import delete_request, get_request
 
 
 @responses.activate
@@ -37,6 +37,58 @@ def test_delete_request_exception(capture):
 
     with pytest.raises(RuntimeError):
         delete_request(test_url)
+
+    assert len(responses.calls) == 1
+    assert responses.calls[0].request.url == test_url
+    capture.check_present()
+
+
+@responses.activate
+def test_get_request():
+    test_url = "http://jsonplaceholder.typicode.com/"
+    test_header = {"Authorization": "token ThIs_Is_A_ToKeN"}
+
+    responses.add(responses.GET, test_url, json={"Response": "OK"}, status=200)
+
+    resp = get_request(test_url, headers=test_header)
+
+    assert len(responses.calls) == 1
+    assert responses.calls[0].request.url == test_url
+    assert responses.calls[0].response.text == '{"Response": "OK"}'
+    assert resp.ok
+
+
+@responses.activate
+def test_get_request_text():
+    test_url = "http://jsonplaceholder.typicode.com/"
+    test_header = {"Authorization": "token ThIs_Is_A_ToKeN"}
+
+    responses.add(responses.GET, test_url, json={"Response": "OK"}, status=200)
+
+    resp = get_request(test_url, headers=test_header, text=True)
+
+    assert len(responses.calls) == 1
+    assert responses.calls[0].request.url == test_url
+    assert resp == '{"Response": "OK"}'
+
+
+@responses.activate
+@log_capture()
+def test_get_request_exception(capture):
+    test_url = "http://josnplaceholder.typicode.com/"
+
+    logger = logging.getLogger()
+    logger.error("Could not reach provided URL")
+
+    responses.add(
+        responses.GET,
+        test_url,
+        body="Could not reach provided URL",
+        status=500,
+    )
+
+    with pytest.raises(RuntimeError):
+        get_request(test_url)
 
     assert len(responses.calls) == 1
     assert responses.calls[0].request.url == test_url
