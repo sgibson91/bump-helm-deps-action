f6ba738b55def77d763ab51e526cbd069562a199
Sarah Gibson <sgibson@turing.ac.uk>
2020-07-29 10:16:53 +0100

    Test add_commit_push with a mock

---
+++ tests/test_github.py
@@ -0,0 +1,70 @@
+import pytest
+import logging
+from unittest.mock import patch
+from testfixtures import log_capture
+from helm_bot.github import add_commit_push
+
+
+@log_capture()
+def test_add_commit_push(capture):
+    filename = "filename.txt"
+    charts_to_update = ["chart-1", "chart-2"]
+    chart_info = {"chart-1": "1.2.3", "chart-2": "4.5.6"}
+    repo_name = "test_repo"
+    target_branch = "test-branch"
+    token = "this_is_a_token"
+
+    logger = logging.getLogger()
+    logger.info("Adding file: %s" % filename)
+    logger.info("Successfully added file: %s" % filename)
+    logger.info("Committing file: %s" % filename)
+    logger.info("Successfully committed file: %s" % filename)
+    logger.info("Pushing commits to branch: %s" % target_branch)
+    logger.info("Successfully pushed changes to branch: %s" % target_branch)
+
+    with patch(
+        "helm_bot.github.run_cmd",
+        return_value={"returncode": 0, "output": "", "err_msg": ""},
+    ) as mock_run_cmd:
+        add_commit_push(
+            filename,
+            charts_to_update,
+            chart_info,
+            repo_name,
+            target_branch,
+            token,
+        )
+
+        assert mock_run_cmd.call_count == 3
+
+    capture.check_present()
+
+
+@log_capture()
+def test_add_commit_push_error(capture):
+    filename = "filename.txt"
+    charts_to_update = ["chart-1", "chart-2"]
+    chart_info = {"chart-1": "1.2.3", "chart-2": "4.5.6"}
+    repo_name = "test_repo"
+    target_branch = "test-branch"
+    token = "this_is_a_token"
+
+    logger = logging.getLogger()
+    logger.info("Adding file: %s" % filename)
+    logger.error("Could not add file: %s" % filename)
+    logger.info("Committing file: %s" % filename)
+    logger.error("Could not commit file: %s" % filename)
+    logger.info("Pushing commits to branch: %s" % target_branch)
+    logger.error("Could not push to branch: %s" % target_branch)
+
+    with pytest.raises(RuntimeError):
+        add_commit_push(
+            filename,
+            charts_to_update,
+            chart_info,
+            repo_name,
+            target_branch,
+            token,
+        )
+
+    capture.check_present()
