c87f8ccd354f55537fd55b92c517793ed555251c
Sarah Gibson <drsarahlgibson@gmail.com>
2021-10-08 15:34:53 +0100

    ✅ Update tests for app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,7 @@
-from helm_bot.app import check_versions
+from helm_bot.app import compare_dependency_versions
 
 
-def test_check_versions_match():
+def test_compare_dependency_versions_match():
     chart_name = "test_chart"
     chart_info = {
         chart_name: {"chart1": "1.2.3", "chart2": "4.5.6"},
@@ -9,12 +9,12 @@ def test_check_versions_match():
         "chart2": "4.5.6",
     }
 
-    charts_out = check_versions(chart_name, chart_info)
+    charts_out = compare_dependency_versions(chart_info, chart_name)
 
     assert charts_out == []
 
 
-def test_check_versions_no_match():
+def test_compare_dependency_versions_no_match():
     chart_name = "test_chart"
     chart_info = {
         chart_name: {"chart1": "1.2.3", "chart2": "4.5.6"},
@@ -23,20 +23,6 @@ def test_check_versions_no_match():
     }
     expected_charts = ["chart1", "chart2"]
 
-    charts_out = check_versions(chart_name, chart_info)
-
-    assert charts_out == expected_charts
-
-
-def test_check_versions_no_match_dry_run():
-    chart_name = "test_chart"
-    chart_info = {
-        chart_name: {"chart1": "1.2.3", "chart2": "4.5.6"},
-        "chart1": "7.8.9",
-        "chart2": "1.10.9",
-    }
-    expected_charts = ["chart1", "chart2"]
-
-    charts_out = check_versions(chart_name, chart_info, dry_run=True)
+    charts_out = compare_dependency_versions(chart_info, chart_name)
 
     assert charts_out == expected_charts
