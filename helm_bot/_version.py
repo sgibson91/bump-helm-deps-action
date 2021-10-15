7e1023f008056433435fde982df4294acab19dc6
Sarah Gibson <drsarahlgibson@gmail.com>
2021-10-15 17:23:51 +0100

    🔖 Add versioning infrastructure

---
+++ helm_bot/_version.py
@@ -0,0 +1,11 @@
+"""
+Provides helm_bot version information.
+"""
+
+# This file is auto-generated! Do not edit!
+# Use `python -m incremental.update helm_bot` to change this file.
+
+from incremental import Version
+
+__version__ = Version("helm_bot", 1, 0, 0)
+__all__ = ["__version__"]
